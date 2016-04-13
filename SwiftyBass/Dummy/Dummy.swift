436affe83bb6e9434a532bc9fc32e0a1999bdec9
Alexander Orlov <me@orlovalex.com>
2016-04-13 19:46:28 +0300

    Trying to fix linking.

--- SwiftyBass/Dummy/Dummy.swift
+++ SwiftyBass/Dummy/Dummy.swift
@@ -1,11 +1,13 @@
 //
 //  Dummy.swift
-//  bass
+//  SwiftyBass
 //
 //  Created by Alexander Orlov on 13/04/16.
 //  Copyright © 2016 Alexander Orlov. All rights reserved.
 //
 
 private struct Dummy {
-    //
+    private func dummy() {
+        BASS_Init(0, 0, 0, nil, nil)
+    }
 }
