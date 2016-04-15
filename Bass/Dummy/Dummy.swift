42bbaaa93be26c4a9138b0b611d1013d7560efad
Alexander Orlov <me@orlovalex.com>
2016-04-15 10:34:56 +0300

    Renamed folders/files.

--- Bass/Dummy/Dummy.swift
+++ Bass/Dummy/Dummy.swift
@@ -1,13 +1,13 @@
 //
 //  Dummy.swift
-//  SwiftyBass
+//  Bass
 //
 //  Created by Alexander Orlov on 13/04/16.
 //  Copyright © 2016 Alexander Orlov. All rights reserved.
 //
 
-public struct __dummy {
-    public func dummy() {
+public struct ___dummy {
+    public func ___dummy() {
         BASS_Init(0, 0, 0, nil, nil)
     }
 }
